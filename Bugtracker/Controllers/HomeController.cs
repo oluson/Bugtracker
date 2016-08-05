1b076f606e0dc19665492d8ea7fe9f01701543b0
oluson <oosajere@gmail.com>
2016-08-05 15:18:14 -0400

    updated

--- Bugtracker/Controllers/HomeController.cs
+++ Bugtracker/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bugtracker.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +9,10 @@ namespace Bugtracker.Controllers
 {
     public class HomeController : Controller
     {
+       // private UserRolesHelper Urh = new UserRolesHelper(new ApplicationDbContext);
         public ActionResult Index()
         {
+        
             return View();
         }
 
---
+++ Bugtracker/Models/UserRolesHelper.cs
@@ -0,0 +1,59 @@
+﻿
+using System.Linq;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+
+namespace Bugtracker.Models
+{
+    public class UserRolesHelper
+    {
+        private ApplicationDbContext db;
+        private UserManager<ApplicationUser> userManager;
+        private RoleManager<IdentityRole> roleManager;
+
+        public UserRolesHelper (ApplicationDbContext context)
+        {
+            userManager = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(context));
+            roleManager = new RoleManager<IdentityRole>(
+                new RoleStore<IdentityRole>(context));
+            db = context;
+        }
+
+        public bool IsUserInRole(string userId, string roleName)
+        {
+            return userManager.IsInRole(userId, roleName);
+        }
+
+        public IList<string> ListUSerRoles(string userId)
+        {
+            return userManager.GetRoles(userId);
+        }
+
+        public bool AddUserToRole(string userId, string roleName)
+        {
+            var result = userManager.AddToRole(userId, roleName);
+            return result.Succeeded;
+        }
+
+        public bool RemoveUserFromRole(string userId, string roleName)
+        {
+            var result = userManager.RemoveFromRole(userId, roleName);
+            return result.Succeeded;
+        }
+
+        public IList<ApplicationUser> UsersInRole(string roleName)
+        {
+            var userIDs = roleManager.FindByName(roleName).Users.Select(r => r.UserId);
+            return userManager.Users.Where(u => userIDs.Contains(u.Id)).ToList();                
+        }
+        public IList<ApplicationUser> UsersNotInRole(string roleName)
+        {
+            var userIDs = System.Web.Security.Roles.GetUsersInRole(roleName);
+            return userManager.Users.Where(u => !userIDs.Contains(u.Id)).ToList();
+        }
+    }
+}
